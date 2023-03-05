1bb75fa2568b070d0fcbde42086efb51fbbfbd7d
Kristian2109 <kristian.petrov1998@gmail.com>
2023-03-05 10:53:05 +0200

    Add the backend for changing a student

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -1,5 +1,22 @@
 const Student = require("../model/database").studentModel;
-const CreateStudent = require("../model/testData").CreateRandomStudent;
+const { templateSettings } = require("lodash");
+const TestData = require("../model/testData");
+const CreateStudent = TestData.CreateRandomStudent;
+const SetPrecision = TestData.GetPrecision;
+const PRECISION = 2;
+
+function GetIndexOfMajorByName(majors, nameToSearch) {
+    for (let i = 0; i < majors.length; i++) {
+        if (majors[i].name === nameToSearch) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function GetAvgGrade(grades) {
+    return grades.reduce((acc, grade) => acc + grade, 0) / grades.length;
+}
 
 async function FindStudents(req, res) {
     try {
@@ -40,8 +57,57 @@ async function FindOneStudent(req, res) {
 }
 
 async function ModifyStudent(req, res) {
-    console.log(req.body);
-    res.redirect("/students");
+    const {studentId, majorAddNote, majorDeleteNote, noteToAdd: noteToAddString, noteToDelete: noteToDeleteString, majorToAdd} = req.body;
+    let student = await Student.findById(studentId);
+
+    if (majorToAdd) {
+        student.disciplines.push({
+            name: majorToAdd,
+            grades: [],
+            avgGradeDisc: 0
+        });
+        await student.save();
+    }
+
+    if (majorDeleteNote) {
+        const indexOfMajorDelete = GetIndexOfMajorByName(student.disciplines, majorDeleteNote);
+        
+        if (indexOfMajorDelete == -1) {
+            return res.render("errorPage", {error: "Invalid  major for deleting note!"});
+        }
+
+        const noteToDelete = Number(noteToDeleteString);
+        if (!noteToDelete || noteToDelete < 2 || noteToDelete > 6) {
+            return res.render("errorPage", {error: "Please enter valid note to delete!"});
+        }
+
+        let majorToModify = student.disciplines[indexOfMajorDelete];
+        majorToModify.grades.remove(noteToDelete);
+        majorToModify.avgGradeDisc = SetPrecision(GetAvgGrade(majorToModify.grades), PRECISION);
+        await student.save();
+    }
+
+    if (majorAddNote) {
+        const indexOfMajorAdd = GetIndexOfMajorByName(student.disciplines, majorAddNote);
+
+        if (indexOfMajorAdd == -1) {
+            return res.render("errorPage", {error: "Invalid  major for adding note!"});
+        }
+
+        const noteToAdd = Number(noteToAddString);
+        if (!noteToAdd || noteToAdd < 2 || noteToAdd > 6) {
+            return res.render("errorPage", {error: "Please enter valid note to add!"});
+        }
+
+        let majorToModify = student.disciplines[indexOfMajorAdd];
+        majorToModify.grades.push(noteToAdd);
+        majorToModify.avgGradeDisc = SetPrecision(GetAvgGrade(majorToModify.grades), PRECISION);
+        await student.save();
+    }
+
+    
+    console.log(student);
+    res.redirect(`/students/${studentId}`);
 }
 
 // function RandomTrueOrFalse() {
